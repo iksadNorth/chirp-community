01a2d9be89dc622c4e93377eb00d0e11236a4c42
iksadnorth <iksadnorth@gmail.com>
2023-05-14 10:28:32 +0900

    #23 - 게시물 도메인 설계 1. article의 각 칼럼은 ID(고유 번호), CreatedAt(생성 일자), Board_id(게시판 ID), Title(제목), Writer_id(작성자 ID), Content(글 내용), Views(조회수)를 의미한다.

---
+++ back_end/src/main/java/com/chirp/community/entity/Article.java
@@ -0,0 +1,46 @@
+package com.chirp.community.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity @Table(name = "article", indexes = {
+        @Index(columnList = "board_id")
+})
+@NoArgsConstructor @Getter @Setter
+@EntityListeners(AuditingEntityListener.class)
+public class Article extends BaseEntity {
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @ManyToOne(cascade = {CascadeType.REMOVE}, fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    @CreatedBy
+    @ManyToOne(cascade = {CascadeType.REMOVE}, fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id", nullable = false)
+    private SiteUser writer;
+
+    @Column(name = "views", nullable = false) @ColumnDefault("0")
+    private Long views;
+
+    public static Article of(Long id, String title, String content) {
+        Article entity = new Article();
+        entity.setId(id);
+        entity.setTitle(title);
+        entity.setContent(content);
+        entity.setViews(0L);
+        return entity;
+    }
+    public static Article of(String title, String content) {
+        return of(null, title, content);
+    }
+}
