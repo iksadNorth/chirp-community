d75373b00a759bb5209a523afb9c5f175134849c
iksadnorth <iksadnorth@gmail.com>
2023-05-14 10:02:22 +0900

    #34 - Jpa Auditing 기능 구현 1. JpaAuditingConfig 설정을 통해 AuditorAware<> 재정의. 2. 각 도메인이 AuditingEntityListener를 적용할 수 있게 @EntityListeners(AuditingEntityListener.class) 어노테이션을 추가함.

---
+++ back_end/src/main/java/com/chirp/community/configuration/JpaAuditingConfig.java
@@ -0,0 +1,26 @@
+ package com.chirp.community.configuration;
+
+import com.chirp.community.entity.SiteUser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+
+    @Bean
+    public AuditorAware<SiteUser> auditorAware() {
+        return () -> Optional.of(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(SiteUser.class::cast);
+    }
+}
