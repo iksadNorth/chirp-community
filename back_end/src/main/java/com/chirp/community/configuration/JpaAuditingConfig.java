5c9143f98a09833098bbc051654f1b9eeec0b7ce
iksadnorth <iksadnorth@gmail.com>
2023-05-15 13:29:06 +0900

    #53 - @CreatedDate 가 작동하지 않는 버그 - 기존의 문제점 Bean 중복 문제 대두.
    - 해결 방안
    JpaAuditingConfig에서 정의되었던 @EnableJpaAuditing 어노테이션 삭제.

--- back_end/src/main/java/com/chirp/community/configuration/JpaAuditingConfig.java
+++ back_end/src/main/java/com/chirp/community/configuration/JpaAuditingConfig.java
@@ -1,18 +1,16 @@
  package com.chirp.community.configuration;
 
-import com.chirp.community.entity.SiteUser;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+ import com.chirp.community.entity.SiteUser;
+ import org.springframework.context.annotation.Bean;
+ import org.springframework.context.annotation.Configuration;
+ import org.springframework.data.domain.AuditorAware;
+ import org.springframework.security.core.Authentication;
+ import org.springframework.security.core.context.SecurityContext;
+ import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.util.Optional;
+ import java.util.Optional;
 
 @Configuration
-@EnableJpaAuditing
 public class JpaAuditingConfig {
 
     @Bean
