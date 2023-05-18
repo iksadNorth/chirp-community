1b047e786244e866f519181cd1b96c397d0fd8bc
iksadnorth <iksadnorth@gmail.com>
2023-05-18 15:55:02 +0900

    #84 - 로그인 페이지 구현 # 구현 중 특이 사항 1. 로그인 실패 시, 실패 원인을 에러 메시지 라벨에 띄움. 2. 로그인 성공 시, 이전 페이지로 넘어감. 3. 회원 가입 페이지로 넘어가는 부분 존재. 4. 로그인 성공 후, JWT 취득 시, LocalStorage에 저장.

---
+++ frontend/src/ComponentsUsers/Login/index.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import './css.css';
+
+import { post } from '../../api';
+import { setToken, isNotBlank } from '../../utils';
+
+import { Link, useNavigate } from 'react-router-dom';
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [error, setError] = useState('');
+
+    const history = useNavigate();
+
+    const wrap = (setFunc) => {
+        return (event) => 
+            setFunc(event.target.value);
+    };
+
+    const handleLogin = () => {
+        post('/api/v1/auth/login', {
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            })
+        })
+        .then((response) => {
+            setToken(response.token);
+            setError(null);
+            history(-1);
+        })
+        .catch((error) => {
+            console.log("handleLogin error");
+            setError(error.errorMessage);
+        })
+    };
+
+  return (
+    <div className="container custom-size custom-gap border rounded-5 shadow">
+        <h1
+            className="fw-bold"
+        >Chirp</h1>
+
+        <div className="spacer" />
+        
+        <div className="input-group input-group-lg justify-content-center">
+            <span className="input-group-text" id="basic-addon1">
+                <i className="bi bi-envelope-at-fill"></i>
+            </span>
+            <input type="email" className="form-control" placeholder="이메일을 입력하세요." value={email} onChange={wrap(setEmail)} />
+        </div>
+        
+        <div className="input-group input-group-lg justify-content-center">
+            <span className="input-group-text" id="basic-addon1">
+                <i className="bi bi-file-lock2-fill"></i>
+            </span>
+            <input type="password" className="form-control" placeholder="비밀 번호를 입력하세요." value={password} onChange={wrap(setPassword)} />
+        </div>
+
+        <div className="d-flex justify-content-center btn-gap">
+            <Link 
+                className="btn btn-outline-dark rounded-pill fw-bold" 
+                to={process.env.REACT_APP_SIGNUP_URL}
+            >회원가입</Link>
+        </div>
+        
+        <div>
+            <button 
+                className="btn btn-dark btn-lg rounded-pill fw-bold" 
+                onClick={handleLogin}
+            >로그인</button>
+        </div>
+        
+        {(error !== '' && isNotBlank(error)) &&
+        <div>
+            <span className="alert alert-danger" role="alert">
+                {error}
+            </span>
+        </div>
+        }
+    </div>
+  );
+}
