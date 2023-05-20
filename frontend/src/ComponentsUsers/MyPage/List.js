ec652d4655277e4e54d7fd35234621508725d86e
iksadnorth <iksadnorth@gmail.com>
2023-05-20 15:25:03 +0900

    #89 - 게시판 컴포넌트 추가. 1. 해당 게시판은 오로지 UI 부분에서만 설정되어 있다. 때문에 재활용성이 강하다. 2. 게시글 목록, 페이지네이션, 검색창으로 이뤄져 있다.

---
+++ frontend/src/ComponentsUsers/MyPage/List.js
@@ -0,0 +1,28 @@
+import Page from './PaginationBar';
+import SearchInput from './SearchInput';
+import ListCom from './ListCom';
+
+export default function List(props) {
+    return (
+        <div className={`container ${props.className}`}>
+            <div className='row'>
+                {/* 게시글 목록 */}
+                <ListCom>
+                    {props.children}
+                </ListCom>
+            </div>
+            <div className='row'>
+                {/* 페이지네이션 */}
+                <Page className="mb-3"
+                    numTotalPages={props.numTotalPages} numPage={props.numPage}
+                    handlePage={props.handlePage}
+                    radius={props.radius}
+                />
+            </div>
+            <div className='row'>
+                {/* 검색창 */}
+                <SearchInput handleSearch={props.handleSearch} />
+            </div>
+        </div>
+    );
+}
