dd83adbff54de3483d0404d3e3d5ca2e34348ca7
junmo95 <junmop95>
2023-05-29 17:44:38 +0900

    #168-게시판 삭제, 수정 구현 여기까지해서 게시글, 댓글 CRUD 기능은 전부 구현되었다.

--- frontend/src/ComponentsBoard/BoardHeader/index.js
+++ frontend/src/ComponentsBoard/BoardHeader/index.js
@@ -5,8 +5,17 @@ import { del } from "../../api";
 function BoardHeader(props) {
 
     const { '*': id } = useParams();
-
     console.log('BoardHeader props:', props);
+
+
+    const handleDeleteArticle = async () => {
+        // 게시글 삭제 API 호출
+        await del(`/api/v1/article/${props.articleId}`);
+
+        // 게시판 페이지로 이동
+        window.location.href = `/board/${props.boardId}`;
+    };
+
 
     return (
         <div className="ArticleHeader">
@@ -16,21 +25,21 @@ function BoardHeader(props) {
                     <a class="navbar-brand" href="#!">{props.boardName} </a>
                     <div>
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            {props.onArticle === 'on' && props.mode === 'on' && (
+                            {props.onArticle === 'on' && props.udMode === 'on' && (
                                 <li className="nav-item">
-                                    <Link to={`/create/article/${props.boardId}`} className="nav-link active" aria-current="page">글 수정</Link>
+                                    <Link to={`/board/${props.boardId}/article/${id}/updateArticle`} className="nav-link active" aria-current="page">글 수정</Link>
                                 </li>
                             )}
-                            {props.onArticle === 'on' && props.mode === 'on' && (
+
+                            {props.onArticle === 'on' && props.udMode === 'on' && (
 
                                 <li className="nav-item">
-                                <Link to={`/board/${props.boardId}`} className="nav-link active" aria-current="page" 
-                                >글 삭제</Link>
-                            </li>
+                                    <Link to={`/board/${props.boardId}`} className="nav-link active" aria-current="page" onClick={handleDeleteArticle}>글 삭제</Link>
+                                </li>
                             )}
 
                             <li className="nav-item">
-                                <Link to={`/create/article/${props.boardId}`} className="nav-link active" aria-current="page">글쓰기</Link>
+                                <Link to={`/board/${props.boardId}/createArticle`} className="nav-link active" aria-current="page">글쓰기</Link>
                             </li>
 
                         </ul>
