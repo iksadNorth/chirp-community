fd01fa09651a13ee4df59084179d411c00a1e821
iksadnorth <iksadnorth@gmail.com>
2023-06-01 23:40:28 +0900

    #188 - 추천 급상승 게시판 구현 # 버그 수정 - 추천 급상승 게시판은 최근 작성글 기준으로 나열할 이유가 없으므로 쿼리에서 해당 부분을 제거함.

--- frontend/src/ComponentsBoard/MainPage/RisingLikeArticle.js
+++ frontend/src/ComponentsBoard/MainPage/RisingLikeArticle.js
@@ -8,12 +8,10 @@ import { addParams, pageRequest } from '../../utils';
 export default function RisingLikeArticle(props) {
     const funcLoadData = (page) => {
         const size = 5;
-        const sort_field = "createdAt";
-        const sort_asc = false;
 
         return get(addParams(
             `/api/v1/article/best/likes`, 
-            pageRequest(page, size, sort_field, sort_asc)
+            pageRequest(page, size)
         ));
     };
 
--- frontend/src/ComponentsBoard/MainPage/RisingViewArticle.js
+++ frontend/src/ComponentsBoard/MainPage/RisingViewArticle.js
@@ -8,12 +8,10 @@ import { addParams, pageRequest } from '../../utils';
 export default function RisingViewArticle(props) {
     const funcLoadData = (page) => {
         const size = 5;
-        const sort_field = "createdAt";
-        const sort_asc = false;
 
         return get(addParams(
             `/api/v1/article/best/views`, 
-            pageRequest(page, size, sort_field, sort_asc)
+            pageRequest(page, size)
         ));
     };
 
